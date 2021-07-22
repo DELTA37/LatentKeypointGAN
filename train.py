673f3af4a4893eba8e01608a987b46597ee24d0d
DELTA_37 <gg_plastt@mail.ru>
2021-07-22 19:48:15 +0300

    fix latent code info

--- train.py
+++ train.py
@@ -91,7 +91,7 @@ def g_path_regularize(fake_img, latents, mean_path_length, decay=0.01):
     grad, = autograd.grad(
         outputs=(fake_img * noise).sum(), inputs=latents, create_graph=True
     )
-    path_lengths = torch.sqrt(grad.pow(2).sum(2).mean(1))
+    path_lengths = torch.sqrt(grad.pow(2).sum(1))
 
     path_mean = mean_path_length + decay * (path_lengths.mean() - mean_path_length)
 
